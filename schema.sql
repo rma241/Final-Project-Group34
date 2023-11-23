14f63830c96875ddd099a685d5e6ccf92bd24344
rma241 <148827901+rma241@users.noreply.github.com>
2023-11-23 17:28:27 -0600

    Cleaning data & Create db and tables

--- schema.sql
+++ schema.sql
@@ -1,1 +1,25 @@
--- Write your tables' schemas in this file
+
+    CREATE TABLE IF NOT EXISTS zip_codes (
+        id INTEGER PRIMARY KEY,
+        zipcode VARCHAR,
+        geometry GEOMETRY
+    )
+    
+    CREATE TABLE IF NOT EXISTS nyc_311 (
+        id INTEGER PRIMARY KEY,
+        created_date DATE,
+        zipcode GEOMETRY
+    )
+    
+    CREATE TABLE IF NOT EXISTS trees (
+        tree_id INTEGER PRIMARY KEY,
+        zipcode VARCHAR,
+        geometry GEOMETRY
+    )
+    
+    CREATE TABLE IF NOT EXISTS rent (
+        id INTEGER PRIMARY KEY,
+        zipcode VARCHAR,
+        average_rent NUMERIC
+    )
+    
