ee8892ca2cc4294a1e7d8e191ddbf55838f8860d
rma241 <148827901+rma241@users.noreply.github.com>
2023-11-24 18:54:33 -0600

    Updated the function used to load data into tables
    slightly changed the function to avoid error message

--- schema.sql
+++ schema.sql
@@ -1,12 +1,11 @@
 
     CREATE TABLE IF NOT EXISTS zip_codes (
-        id INTEGER PRIMARY KEY,
         zipcode VARCHAR,
         geometry GEOMETRY
     )
     
     CREATE TABLE IF NOT EXISTS nyc_311 (
-        id INTEGER PRIMARY KEY,
+        unique_key INTEGER PRIMARY KEY,
         created_date DATE,
         zipcode GEOMETRY
     )
@@ -18,7 +17,7 @@
     )
     
     CREATE TABLE IF NOT EXISTS rent (
-        id INTEGER PRIMARY KEY,
+        RegionID INTEGER PRIMARY KEY,
         zipcode VARCHAR,
         average_rent NUMERIC
     )
