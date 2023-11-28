f812904f7b7a969b01c84f3cb9d3eb6eb0a91087
rma241 <148827901+rma241@users.noreply.github.com>
2023-11-28 18:58:58 -0500

    Delete queries/Q1_311compliants_per_zipcode.sql

--- queries/Q1_311compliants_per_zipcode.sql
+++
@@ -1,6 +0,0 @@
-
-SELECT incident_zip, COUNT(unique_key) as total_complaints
-FROM nyc_311
-WHERE created_date BETWEEN '2022-10-01' AND '2023-09-30'
-GROUP BY incident_zip
-ORDER BY total_complaints DESC;
