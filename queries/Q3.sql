f470921fd48500931e36d754d8168209357033be
rma241 <148827901+rma241@users.noreply.github.com>
2023-11-27 19:40:46 -0500

    Delete queries/Q3.sql

--- queries/Q3.sql
+++
@@ -1,8 +0,0 @@
-
-SELECT t.zipcode, 
-       ROUND(CAST(r."2023-08-31" AS NUMERIC), 2) AS August2023
-FROM trees t
-JOIN rent r ON t.zipcode = r."Zip_code"
-GROUP BY t.zipcode, r."2023-08-31"
-ORDER BY COUNT(t.tree_id) DESC
-LIMIT 10;
