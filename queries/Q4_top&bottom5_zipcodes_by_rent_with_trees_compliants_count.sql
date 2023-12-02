5418026a5c5d9ea69db6347b40c8c17d1e46a298
rma241 <148827901+rma241@users.noreply.github.com>
2023-12-01 19:43:41 -0500

    Update Project.ipynb, README.md, requirements.txt
    - for the Project.ipynb: added comments, descriptions and explanations and delete some unused cells
    - edited README.md and requirements.txt as required

--- queries/Q4_top&bottom5_zipcodes_by_rent_with_trees_compliants_count.sql
+++ queries/Q4_top&bottom5_zipcodes_by_rent_with_trees_compliants_count.sql
@@ -1,27 +1,38 @@
 
-WITH subquery AS (SELECT rent.zipcode, rent."2023-01-31", total_trees, total_complaints
-FROM rent
-JOIN (SELECT nyc_311.zipcode, COUNT(*) as total_complaints
+WITH subquery AS 
+(
+    SELECT rent.zipcode, rent."2023-01-31", total_trees, total_complaints
+    FROM rent
+    JOIN 
+    (
+        SELECT nyc_311.zipcode, COUNT(*) as total_complaints
             FROM nyc_311
             WHERE created_date BETWEEN '2023-01-01' AND '2023-01-31'
             GROUP BY nyc_311.zipcode
-) nyc_311
-ON rent.zipcode = nyc_311.zipcode
-JOIN (SELECT trees.zipcode, COUNT(trees.tree_id) as total_trees
+    ) nyc_311 ON rent.zipcode = nyc_311.zipcode
+    JOIN 
+    (
+        SELECT trees.zipcode, COUNT(trees.tree_id) as total_trees
             FROM trees
             GROUP BY trees.zipcode
-) trees
-ON rent.zipcode = trees.zipcode
-WHERE rent."2023-01-31" IS NOT NULL
+    ) trees ON rent.zipcode = trees.zipcode
+    WHERE rent."2023-01-31" IS NOT NULL
 ) 
 
-(SELECT zipcode, ROUND(CAST("2023-01-31" AS NUMERIC),2), total_trees, total_complaints 
-FROM subquery
+SELECT zipcode, ROUND(CAST("2023-01-31" AS NUMERIC),2), total_trees, total_complaints FROM
+(
+    (
+        SELECT *
+        FROM subquery
+        ORDER BY "2023-01-31" DESC
+        LIMIT 5
+    )
+    UNION
+    (
+        SELECT * 
+        FROM subquery
+        ORDER BY "2023-01-31" ASC
+        LIMIT 5
+    )
+)
 ORDER BY "2023-01-31" DESC
-LIMIT 5)
-UNION
-(SELECT zipcode, ROUND(CAST("2023-01-31" AS NUMERIC),2), total_trees, total_complaints 
-FROM subquery
-ORDER BY "2023-01-31" ASC
-LIMIT 5)
-
--- queries/Q6_trees_within_0.5miles.sql
+++ queries/Q6_trees_within_0.5miles.sql
@@ -1,4 +1,4 @@
 
-SELECT tree_id, species, health, the_geom
+SELECT tree_id, species, health, (ST_X(the_geom), ST_Y(the_geom))
 FROM trees
 WHERE ST_DWithin(ST_GeomFromText('POINT(-73.96253174434912 40.80737875669467)',4326), the_geom::geography, 804.672)
